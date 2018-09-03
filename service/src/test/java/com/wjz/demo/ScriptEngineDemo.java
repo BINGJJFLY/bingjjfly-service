1ff5ba14d2e9d04ddfcffcb2ed3403226e445ea3
wangjz <17091267797@163.com>
2018-09-03 21:41:48 +0800

    字符串参与运算

--- service/src/test/java/com/wjz/demo/ScriptEngineDemo.java
+++ service/src/test/java/com/wjz/demo/ScriptEngineDemo.java
@@ -8,9 +8,6 @@ import javax.script.ScriptException;
 
 /**
  * 字符串执行算数运算、数值保留小数位及四舍五入方式
- * <p>
- * 但是中运算不能保留具体的小数位，不能四舍五入
- * </p>
  * 
  * @author iss002
  *
@@ -19,11 +16,11 @@ public class ScriptEngineDemo {
 
 	public static void main(String[] args) {
 		ScriptEngine jse = new ScriptEngineManager().getEngineByName("JavaScript");
-		String s = "((1+1*2)+2/5)*3+3+0.8";
+		String s = "1.5603+3.8934*3.67/5.89";
 		try {
 			Double d = (Double) jse.eval(s);
 			System.out.println(d);
-			BigDecimal bd = new BigDecimal(19.0896725D);
+			final BigDecimal bd = new BigDecimal(d);
 			System.out.println(bd.setScale(4, BigDecimal.ROUND_HALF_UP).doubleValue());
 		} catch (ScriptException e) {
 			e.printStackTrace();
