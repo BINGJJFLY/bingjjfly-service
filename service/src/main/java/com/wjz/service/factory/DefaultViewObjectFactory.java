3fc56eecd4f5a436a588cf27904108faa64c35ed
wjz <17091267797@163.com>
2018-10-23 14:05:44 +0800

    Dao层转视图层工厂

--- service/src/main/java/com/wjz/service/factory/DefaultViewObjectFactory.java
+++ service/src/main/java/com/wjz/service/factory/DefaultViewObjectFactory.java
@@ -17,6 +17,8 @@ import java.util.Set;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.SystemMetaObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cglib.core.ReflectUtils;
 import org.springframework.core.annotation.AnnotationUtils;
 
@@ -26,7 +28,18 @@ import com.wjz.service.annotation.ViewProperty;
 import com.wjz.service.exception.UnAssignableException;
 import com.wjz.service.utils.DateUtils;
 
+/**
+ * <b>默认Dao层对象转视图层对象工厂</b>
+ * <p>
+ * 支持Dao层对象类型、Dao层对象集合类型、Dao层对象Map类型
+ * </p>
+ * 
+ * @author iss002
+ *
+ */
 public class DefaultViewObjectFactory extends ViewObjectFactory {
+
+	private static final Logger log = LoggerFactory.getLogger(DefaultViewObjectFactory.class);
 
 	@Override
 	public Object transform(Object domain) throws UnAssignableException {
@@ -35,34 +48,32 @@ public class DefaultViewObjectFactory extends ViewObjectFactory {
 		if (domain != null) {
 			Class<?> domainType = domain.getClass();
 			MetaObject domainMetaObject = SystemMetaObject.forObject(domain);
-			// 判断对象是否为集合对象
-			boolean isCollection = domainMetaObject.isCollection();
-			// 集合对象或Map对象
-			if (isCollection || isMap(domainType)) {
-				if (List.class.isAssignableFrom(domainType)) {
-					Collection<?> dos = ((Collection<?>) domain);
-					Iterator<?> iterator = dos.iterator();
+			// 集合对象
+			if (isAssignableFrom(Collection.class, domainType)) {
+				Collection<?> dos = ((Collection<?>) domain);
+				Iterator<?> iterator = dos.iterator();
+				if (isAssignableFrom(List.class, domainType)) {
 					list = new ArrayList<>(((List<?>) domain).size());
 					while (iterator.hasNext()) {
 						list.add(transform(iterator.next()));
 					}
 					return list;
-				} else if (Set.class.isAssignableFrom(domainType)) {
-					Collection<?> dos = (Collection<?>) domain;
-					Iterator<?> iterator = dos.iterator();
+				} else if (isAssignableFrom(Set.class, domainType)) {
 					set = new HashSet<>();
 					while (iterator.hasNext()) {
 						set.add(transform(iterator.next()));
 					}
 					return set;
-				} else if (Map.class.isAssignableFrom(domainType)) {
-					Map<?, ?> domap = (Map<?, ?>) domain;
-					Map<Object, Object> map = new HashMap<>(domap.size());
-					for(Map.Entry<?, ?> m : domap.entrySet()) {
-						map.put(m.getKey(), transform(m.getValue()));
-					}
-					return map;
 				}
+			}
+			// Map对象
+			else if (isAssignableFrom(Map.class, domainType)) {
+				Map<?, ?> domap = (Map<?, ?>) domain;
+				Map<Object, Object> map = new HashMap<>(domap.size());
+				for (Map.Entry<?, ?> m : domap.entrySet()) {
+					map.put(m.getKey(), transform(m.getValue()));
+				}
+				return map;
 			}
 			// 普通Bean对象
 			else {
@@ -78,67 +89,26 @@ public class DefaultViewObjectFactory extends ViewObjectFactory {
 								// 排除静态字段
 								if (!Modifier.isStatic(field.getModifiers())) {
 									String fieldName = field.getName();
-									Object fieldValue = domainMetaObject.getValue(field.getName());
-									// 声明字段类型不是集合类型
-									if (!isCollection(field.getType())) {
-										String name = null;
-										String pattern = null;
-										// 检查字段上是否有@ViewProperty注解
-										ViewProperty viewPropertyAnno = AnnotationUtils.getAnnotation(field,
-												ViewProperty.class);
-										if (viewPropertyAnno != null) {
-											name = viewPropertyAnno.name();
-											pattern = viewPropertyAnno.pattern();
-										}
-										// 处理字段名称不匹配
-										if (!StringUtils.isEmpty(name)) {
-											fieldName = name;
-										}
-										// 处理日期类型，有默认格式
-										if (Date.class.isAssignableFrom(field.getType())) {
-											if (fieldValue != null) {
-												if (StringUtils.isEmpty(pattern)) {
-													pattern = DEFAULT_DATE_PATTERN;
-												}
-												fieldValue = DateUtils.format((Date) fieldValue, pattern);
-											}
-											setValue(fieldName, fieldValue, viewMetaObject);
-										}
-										// 处理数值类型，没有默认格式
-										else if (Number.class.isAssignableFrom(field.getType())) {
-											if (fieldValue != null) {
-												if (!StringUtils.isEmpty(pattern)) {
-													fieldValue = new DecimalFormat(pattern).format(fieldValue);
-												} else {
-													fieldValue = String.valueOf(fieldValue);
-												}
-											}
-											setValue(fieldName, fieldValue, viewMetaObject);
-										}
-										// Map类型
-										else if (Map.class.isAssignableFrom(field.getType())) {
-											Map<?, ?> map = (Map<?, ?>) domainMetaObject.getValue(fieldName);
-											setValue(fieldName, transform(map), viewMetaObject);
-										}
-										// 其他类型
-										else {
-											if (fieldValue != null) {
-												fieldValue = String.valueOf(fieldValue);
-											}
-											setValue(fieldName, fieldValue, viewMetaObject);
-										}
+									Object fieldValue = getValue(field.getName(), domainMetaObject);
+									Class<?> fieldType = field.getType();
+									String name = null;
+									String pattern = null;
+									// 检查字段上是否有@ViewProperty注解
+									ViewProperty propertyAnno = AnnotationUtils.getAnnotation(field,
+											ViewProperty.class);
+									if (propertyAnno != null) {
+										name = propertyAnno.name();
+										pattern = propertyAnno.pattern();
 									}
-									// 声明字段类型是集合类型
-									else {
-										Collection<?> collection = (Collection<?>) domainMetaObject.getValue(fieldName);
-										setValue(fieldName, transform(collection), viewMetaObject);
+									// 处理字段名称不匹配
+									if (!StringUtils.isEmpty(name)) {
+										fieldName = name;
 									}
+									handle(fieldType, fieldName, fieldValue, pattern, domainMetaObject, viewMetaObject);
 								}
 							}
 						}
 						return viewObject;
-					} else {
-						throw new UnAssignableException("domain's viewObjectType must not be null.");
 					}
 				}
 			}
@@ -146,10 +116,69 @@ public class DefaultViewObjectFactory extends ViewObjectFactory {
 		return domain;
 	}
 
-	private void setValue(String name, Object value, MetaObject viewMetaObject) {
-		if (value != null) {
-			viewMetaObject.setValue(name, value);
+	private void handle(Class<?> fieldType, String fieldName, Object fieldValue, String pattern,
+			MetaObject domainMetaObject, MetaObject viewMetaObject) {
+		try {
+			// 处理日期类型，有默认格式
+			if (isAssignableFrom(Date.class, fieldType)) {
+				if (fieldValue != null) {
+					if (StringUtils.isEmpty(pattern)) {
+						pattern = DEFAULT_DATE_PATTERN;
+					}
+					fieldValue = DateUtils.format((Date) fieldValue, pattern);
+				}
+				setValue(fieldName, fieldValue, viewMetaObject);
+			}
+			// 处理数值类型，没有默认格式
+			else if (isAssignableFrom(Number.class, fieldType)) {
+				if (fieldValue != null) {
+					if (!StringUtils.isEmpty(pattern)) {
+						fieldValue = new DecimalFormat(pattern).format(fieldValue);
+					} else {
+						fieldValue = String.valueOf(fieldValue);
+					}
+				}
+				setValue(fieldName, fieldValue, viewMetaObject);
+			}
+			// 声明字段类型是集合类型
+			else if (isAssignableFrom(Collection.class, fieldType)) {
+				Collection<?> collection = (Collection<?>) domainMetaObject.getValue(fieldName);
+				setValue(fieldName, transform(collection), viewMetaObject);
+			}
+			// 处理Map类型
+			else if (isAssignableFrom(Map.class, fieldType)) {
+				Map<?, ?> map = (Map<?, ?>) domainMetaObject.getValue(fieldName);
+				setValue(fieldName, transform(map), viewMetaObject);
+			}
+			// 处理其他类型
+			else {
+				if (fieldValue != null) {
+					fieldValue = String.valueOf(fieldValue);
+				}
+				setValue(fieldName, fieldValue, viewMetaObject);
+			}
+		} catch (Exception e) {
+			log.error("exception occurs when domain object [" + domainMetaObject.getOriginalObject()
+					+ "] converting to view object.", e);
+			throw new UnAssignableException();
 		}
+	}
+
+	private Object getValue(String field, MetaObject domainMetaObject) {
+		return domainMetaObject.getValue(field);
+	}
+
+	private void setValue(String field, Object value, MetaObject viewMetaObject) {
+		if (value != null) {
+			viewMetaObject.setValue(field, value);
+		}
+	}
+
+	private boolean isAssignableFrom(Class<?> interfaceType, Class<?> classType) {
+		if (interfaceType.isAssignableFrom(classType)) {
+			return true;
+		}
+		return false;
 	}
 
 }
--- service/src/main/java/com/wjz/service/factory/ViewObjectFactory.java
+++ service/src/main/java/com/wjz/service/factory/ViewObjectFactory.java
@@ -1,27 +1,20 @@
 package com.wjz.service.factory;
 
-import java.util.Collection;
-import java.util.Map;
-
 import com.wjz.service.exception.UnAssignableException;
 
+/**
+ * <b>Dao层对象转视图层对象工厂</b>
+ * <p>
+ * 功能：Dao层对象传入被转换为试图层对象
+ * </p>
+ * 
+ * @author iss002
+ *
+ */
 public abstract class ViewObjectFactory {
-	
+
 	protected static final String DEFAULT_DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
 	public abstract Object transform(Object domain) throws UnAssignableException;
-	
-	public boolean isCollection(Class<?> type) {
-		if (Collection.class.isAssignableFrom(type)) {
-			return true;
-		}
-		return false;
-	}
-	
-	public boolean isMap(Class<?> type) {
-		if (Map.class.isAssignableFrom(type)) {
-			return true;
-		}
-		return false;
-	}
+
 }
